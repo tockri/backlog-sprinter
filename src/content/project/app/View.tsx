89323256c25c4fbb42dfb0e8dcf4b6653445c068
fujita <fujita@nulab.com>
2023-01-23 18:46:03 +0900

    wip

--- src/content/project/app/View.tsx
+++ src/content/project/app/View.tsx
@@ -4,16 +4,43 @@ import { Modal } from "../../ui/Modal"
 import { TabPanel } from "../../ui/TabPanel"
 import { useProjectAppLogic } from "./Logic"
 
+import { useAtom } from "jotai"
 import { ProjectProductBacklog } from "../productBacklog/View"
 import { ProjectSettings } from "../settings/View"
 import { PBFormInfo } from "../types"
 import { i18n } from "./i18n"
+import { formInfoAtom } from "./State"
 
 type ProjectAppProps = {
   broker: MessageBroker<PBFormInfo>
 }
 
 export const ProjectApp: React.FC<ProjectAppProps> = (props) => {
+  const { broker } = props
+  const [formInfo, setFormInfo] = useAtom(formInfoAtom)
+  React.useEffect(() => {
+    if (!formInfo) {
+      broker.subscribe("Project", (formInfo) => {
+        setFormInfo(formInfo)
+      })
+    }
+    return () => {
+      broker.unsubscribe("Project")
+    }
+  }, [formInfo, broker])
+  if (formInfo) {
+    const t = i18n(formInfo.lang)
+    return (
+      <Modal onClose={() => setFormInfo(null)} size="large" title={t.formTitle} height="calc(100vh - 200px)">
+        projectKey = {formInfo.projectKey}
+      </Modal>
+    )
+  } else {
+    return <></>
+  }
+}
+
+export const ProjectApp_orig: React.FC<ProjectAppProps> = (props) => {
   const { broker } = props
   const vm = useProjectAppLogic()
   React.useEffect(() => {
